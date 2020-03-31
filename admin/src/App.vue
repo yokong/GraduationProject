5e5fce0bcbf37b7143ceb32fb489e168efa80218
ColorInk <657715602@qq.com>
2020-03-31 19:07:20 +0800

    大改前最后一版

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -5,17 +5,7 @@
 </template>
 
 <style>
-html {
-  overflow-y: scroll;
-}
-html,
-body {
-  margin: 0;
-  padding: 0;
-  background-color: #ffffff;
-}
 .wrapper {
-  height: 100vh;
   overflow-x: hidden;
 }
 </style>
